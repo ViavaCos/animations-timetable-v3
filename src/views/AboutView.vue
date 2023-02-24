f4cb6cbe337e53dd4b66ab8cfa88f26b5ab6653f
Viavacos <Viavacos@hotmail.com>
2023-02-24 10:03:40 +0800

    新增vueRepl

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <Repl :store="store" />
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
+    min-height: calc(100vh - 24px);
     display: flex;
     align-items: center;
+    overflow: hidden;
   }
 }
+.vue-repl {
+  width: 100vw;
+  height: calc(100vh - 24px);
+}
 </style>
+
+<script setup lang="ts">
+import { Repl, ReplStore } from "@vue/repl";
+import "@vue/repl/style.css";
+
+const store = new ReplStore({
+  showOutput: true,
+  outputMode: "preview",
+});
+
+// store.setVueVersion("3.2.8");
+</script>
